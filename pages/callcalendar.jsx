495c04273a553beb0d7c7e5ad1e459499c8b4e77
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-03-20 19:54:53 +0000

    Map the array

--- pages/callcalendar.jsx
+++ pages/callcalendar.jsx
@@ -2,6 +2,7 @@ import { Calendar, People, Job, Equipements } from "@/src/models";
 import { DataStore } from "aws-amplify";
 import { useEffect, useState } from "react";
 import React from "react";
+import { Card } from "@aws-amplify/ui-react";
 
 export default function CallCalendar() {
     const [mycalendar, setMycalendar] = useState([])
@@ -14,38 +15,58 @@ export default function CallCalendar() {
     console.log("useeffect")
     useEffect(() => {
         async function GetDays() {
+
             console.log("cheguei a function callday")
             try {
                 const days = await DataStore.query(Calendar,);
-                setMycalendar(days)
+                setPeople(days)
 
-                const person = await mycalendar[0].people
-                setPeople(person)
-                console.log(person)
 
+
+                console.log("estou no promise", promisedetals)
             } catch (error) {
                 console.log("Error don't get the callday", error);
+            }
+            const promisedetals = await Promise.all(people.map(async (daysofCalendar) => {
+
+                return {
+                    day: daysofCalendar.day,
+                    people: await daysofCalendar.people,
+                    job: await daysofCalendar.job,
+                    equipement: await daysofCalendar.equipement,
+                };
 
             }
+            ))
+            setMycalendar(promisedetals)
+
+
+            console.log("test1")
+
         }
         GetDays()
     }, [])
+    setMycalendar(promisedetals)
 
-    console.log(models)
+    console.log(people)
     console.log("test4.1")
+    console.log("mycalendar", mycalendar)
+    console.log(models)
 
-    console.log(mycalendar)
 
     return (<><div> {mycalendar.map((item) => (
         <li key={item.id}>
             <p>day: {item.day}</p>
-            <p>people: {item.calendarPeopleId}</p>
-            <p>job: {item.calendarJobId}</p>
-            <p>equipement: {item.calendarEquipementId}</p>
-            <p>job: {item.id}</p>
+            <p>people: {item.people}</p>
+            <p>job: {item.job}</p>
+            <p>equipement: {item.equipement}</p>
+
+            <p>id: {item.id}</p>
 
         </li>
     ))}
+        <Card>{models}</Card>
+
 
 
     </div>
