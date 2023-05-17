4005a61ad0a9c2062b67202e2e2d10195083ddbc
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-05-17 22:37:52 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,8 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
 
-import awsconfig from 'aws-amplify';
+
+import awsconfig from '../src/aws-exports';
 
 import "@aws-amplify/ui-react/styles.css";
 import { studioTheme } from "../src/ui-components";
